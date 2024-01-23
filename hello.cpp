841746b02bcff6eba8f2dc605b58e7a45ae07a47
vben <tjdcksl20@gmail.com>
2024-01-23 15:18:03 +0900

    v0.1day01

---
+++ hello.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    //선언
+    int i0;
+    int i1;
+    int sum;
+
+    int ia;
+    double a;
+    const double PI = 3.14;
+    /*
+    //입력받기
+    cout << "첫 번째 숫자 입력 : " << endl;
+    cin >> num1;
+    cout << "두 번째 숫자 입력 : " << endl;
+    cin >> num2;
+    // 계산과 결과 저장
+    sum = num1 + num2;
+    // 출력
+    cout << "두 숫자의 합: " << sum;
+    */
+    i0 = static_cast<double>(i0)/static_cast<double>(i1);
+    a = static_cast<double>(i0)/static_cast<double>(i1);  //static_cast <변환할 것>: <$$>으로 형변환
+    cout << "a = 0/i1=" << a << endl;
+    cout << "a/PI=" << a/PI << endl;
+    cout << "ia=i0/i1=" << ia << endl;
+    return 0;
+}
