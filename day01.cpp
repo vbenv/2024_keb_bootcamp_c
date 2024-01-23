a95b1998f6e05ef42d662da0392769df7f018c00
vbenv <tjdcksl20@gmail.com>
2024-01-23 21:01:08 +0900

    v0.5 day01

--- day01.cpp
+++ day01.cpp
@@ -1,42 +1,33 @@
-// 부호 없는 정수 사용하기
+// 부호 있는 정수 사용
+// 3회 거래 후의 계좌 잔액 구하는 프로그램
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    // 상수 정의
-    const unsigned int pennyValue = 1;
-    const unsigned int nickelValue = 5;
-    const unsigned int dimeValue = 10;
-    const unsigned int quarterValue = 25;
-    const unsigned int dollarValue = 100;
+    // 변수 선언
+    int balance =0;
+    int transaction;
    
-    // 변수 정의(각 코인의 수)
-    unsigned int pennies;
-    unsigned int nickel;
-    unsigned int dime;
-    unsigned int quarter;
-    unsigned int dollar;
-   
-    // 전체 값을 나타내는 변수 선언
-    unsigned long totalValue;
-   
-   // 코인 입력받기
-    cout << "페니의 수: ";
-    cin >> pennies;
-    cout << "니켈의 수: ";
-    cin >> nickel;
-    cout << "다임의 수: ";
-    cin >> dime;
-    cout << "쿼터의 수: ";
-    cin >> quarter;
-    cout << "달러의 수: ";
-    cin >> dollar;
-    
-    // 전체 금액 계산
-    totalValue = pennies * pennyValue + nickel * nickelValue + dime * dimeValue + quarter * quarterValue + dollar * dollarValue;
-    
-    // 결과 출력
-    cout << "전체 값은 " << totalValue << "페니입니다.";
+    // 첫 번째 거래 후 잔액 조정
+    cout << "첫 번째 거래 금액 입력: ";
+    cin >> transaction;
+    balance = balance + transaction;
+    //cout << "첫 번째 거래 후 잔액: " << balance << endl;
+
+    // 두 번째 거래 후 잔액 조정
+    cout << "두 번째 거래 금액 입력: ";
+    cin >> transaction;
+    balance = balance + transaction;
+    //cout << "두 번째 거래 후 잔액: " << balance << endl;
+
+    // 세 번째 거래 후 잔액 조정
+    cout << "세 번째 거래 금액 입력: ";
+    cin >> transaction;
+    balance = balance + transaction;
+    //cout << "세 번째 거래 후 잔액: " << balance << endl;
+
+    // 최종 잔액 출력
+    cout << "계좌의 최종 잔액은 " << balance << "달러입니다.";
     return 0;
 }
