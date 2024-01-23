430f138577afcbbffaf5d233f518fd0685768c14
vbenv <tjdcksl20@gmail.com>
2024-01-23 21:20:19 +0900

    v0.6 day01

--- day01.cpp
+++ day01.cpp
@@ -1,33 +1,12 @@
-// 부호 있는 정수 사용
-// 3회 거래 후의 계좌 잔액 구하는 프로그램
+// 정수 자료형의 크기 확인하기
+// 3가지 정수 자료형의 크기 확인하는 프로그램
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    // 변수 선언
-    int balance =0;
-    int transaction;
-   
-    // 첫 번째 거래 후 잔액 조정
-    cout << "첫 번째 거래 금액 입력: ";
-    cin >> transaction;
-    balance = balance + transaction;
-    //cout << "첫 번째 거래 후 잔액: " << balance << endl;
-
-    // 두 번째 거래 후 잔액 조정
-    cout << "두 번째 거래 금액 입력: ";
-    cin >> transaction;
-    balance = balance + transaction;
-    //cout << "두 번째 거래 후 잔액: " << balance << endl;
-
-    // 세 번째 거래 후 잔액 조정
-    cout << "세 번째 거래 금액 입력: ";
-    cin >> transaction;
-    balance = balance + transaction;
-    //cout << "세 번째 거래 후 잔액: " << balance << endl;
-
-    // 최종 잔액 출력
-    cout << "계좌의 최종 잔액은 " << balance << "달러입니다.";
+    cout << "short int의 크기는 " << sizeof(short int) << "바이트입니다." << endl;
+    cout << "int의 크기는 " << sizeof(int) << "바이트입니다." << endl;
+    cout << "long int의 크기는 " << sizeof(long int) << "바이트입니다." << endl;
     return 0;
 }
