737dda0cf0a7c1ad20bfcce4e77b0dba88b1b303
vbenv <tjdcksl20@gmail.com>
2024-01-24 10:05:33 +0900

    v0.7 day02

--- day01.cpp
+++ day01.cpp
@@ -1,12 +1,17 @@
-// 정수 자료형의 크기 확인하기
-// 3가지 정수 자료형의 크기 확인하는 프로그램
+// 정수 리터럴로 변수 초기화하기
+// 변수 초기화 시 리터럴 사용하는 프로그램
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    cout << "short int의 크기는 " << sizeof(short int) << "바이트입니다." << endl;
-    cout << "int의 크기는 " << sizeof(int) << "바이트입니다." << endl;
-    cout << "long int의 크기는 " << sizeof(long int) << "바이트입니다." << endl;
+    int x = -1234;
+    unsigned int y = 1234;
+    unsigned int z = -236;
+    unsigned int t = 14.56;
+
+    cout << x <<endl;
+    cout << y << endl;
+    cout << z << endl;
     return 0;
 }
---
+++ dya02.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //Multiplication
+    cout << "Testing multiplication operator" << endl;
+    cout << "Value of 3 * 4 = " << 3*4 << endl;
+    cout << "Value of 2.4 * 4.1 = " << 2.4 * 4.1 << endl;
+    cout << "Value of -3 * 1.1 = " << -3 * 1.0 << endl;
+    return 0;
+}
