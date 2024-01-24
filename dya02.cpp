4dbfcf497b624622d83a4d082cfe19e222a89d99
vbenv <tjdcksl20@gmail.com>
2024-01-24 10:47:12 +0900

    v0.7 day02

--- dya02.cpp
+++ dya02.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
+#include <limits>
 using namespace std;
 
 int main()
 {
-    //Multiplication
-    cout << "Testing multiplication operator" << endl;
-    cout << "Value of 3 * 4 = " << 3*4 << endl;
-    cout << "Value of 2.4 * 4.1 = " << 2.4 * 4.1 << endl;
-    cout << "Value of -3 * 1.1 = " << -3 * 1.0 << endl;
+    unsigned int num1 = numeric_limits<unsigned int>::max();
+    cout << num1 <<endl;
+    num1 += 1;
+    cout << num1;
     return 0;
 }
