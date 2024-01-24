5e3fcf738dac2201c66785192a68f65b9efbbdec
vbenv <tjdcksl20@gmail.com>
2024-01-24 14:37:15 +0900

    v1.1 day02

--- dya02.cpp
+++ dya02.cpp
@@ -1,26 +1,20 @@
-// 숫자에서 정수 부분과 소수점 아래 부분을 분리해서 추출하는 프로그램
-// 주어진 부동 소수점의 정수 부분과 소수점 아래 부분을 추출해서 출력
+// 정수 첫 번째 자리수 추출
+// 입력된 정수의 첫 번째 자리수를 추출해서 출력
 
 #include <iostream>
-#include <iomanip> //이게 뭐지
 using namespace std;
 
 int main()
 {
-    // 변수 선언
-    double number;
-    int intPart;
-    double fractPart;
-    // 입력 받기
-    cout << "부동 소수점 입력: ";
-    cin >> number;
-    // 처리
-    intPart = static_cast<int>(number);
-    fractPart = number - intPart;
-    // output
-    cout << fixed << showpoint << setprecision(3); // 이 기능에 반올림 포함.
-    cout << "원래 값: " << number << endl;
-    cout << "정수 부분: " << intPart << endl;
-    cout << "소수점 아래 부분: " << fractPart << endl;
-    return 0;
+   // set variable
+   unsigned int givenInt, firstDigit;
+   // input
+   cout << "input +int: ";
+   cin >> givenInt;
+   // process
+   firstDigit = givenInt % 10; // get a remainder from 10's remainder
+   // output
+   cout << "inputed int: " << givenInt << endl;
+   cout << "extract first position: " << firstDigit << endl;
+   return 0;
 }
