7f34d989dba8aa3ab38d40adc9562f398a26289f
vbenv <tjdcksl20@gmail.com>
2024-01-25 09:29:25 +0900

    v1.4 day03

--- day03.cpp
+++ day03.cpp
@@ -1,16 +1,25 @@
 //반복문 사용해서 메시지 10회 출력
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main()
-{   //카운터 선언 후 초기화.
-    int counter = 0;
+{   //선언 후 초기화.
+    int score;
+    int sum =0;
+    double average; // score를 더블로 바꿔야 소수점 아래가 나오기 때문에 double로 선언. if, int로 선언하면, 계산한 값을 average에 저장할 때 int로 바꿔 소수점 아래를 다 없애고 계산하게됨.
     // while
-    while(counter<10) //10회 반복
+    int counter = 0;
+    while(counter<4) //4 반복
     {
-        cout<<"hello" << endl;
+        cout<<"input your score: ";
+        cin >> score;
+        sum = sum + score;
         counter++;
     }
+    average = static_cast <double>(sum) / 4;  //sum or 나누는 '4' 중 하나를 double(float) 타입으로 바꿔야 묵시적 형변환이 일어나 보다 정확한 수치를 알 수 있음.
+    cout << fixed << setprecision(2) << showpoint;
+    cout << "average score = " << average;
     return 0;
 }
