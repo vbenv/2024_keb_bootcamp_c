b42849d5f1873cfee6694faf7b96b081f62fa950
vbenv <tjdcksl20@gmail.com>
2024-01-29 18:52:58 +0900

    v2.4 day05

--- day05.cpp
+++ day05.cpp
@@ -1,18 +1,31 @@
-// Array 값 swap
+// 배열을 함수로 받아 배열의 요소들 변경하기
 #include <iostream>
 using namespace std;
 
+void multiplyByTwo(int numbers[], int size)
+{
+	for(int i = 0; i < size; i++)
+	{
+		numbers[i] *= 2;
+	}
+	return;
+}
+
+void print(const int numbers[], int size)
+{
+	for(int i = 0; i < size; i++)
+	{
+		cout << numbers[i] << " ";
+	}
+	cout << endl;
+	return;
+}
+
 int main()
 {
-	//declare
-	int numArray[10] = {14, 76, 80, 33, 21, 96, 22, 88, 31, 16};
-    
-	
-	int duck =  numArray[1];
-	numArray[1] = numArray[2];
-	numArray[2] = duck;
-	cout << numArray[1] << endl;//원래 : 76
-	cout << numArray[2] << endl;//원래 : 80
-	cout << duck;
+	int numbers[5] = {150, 170, 180, 120, 130};
+	print(numbers, 5);
+	multiplyByTwo(numbers, 5);
+	print(numbers, 5);
 	return 0;
 }
