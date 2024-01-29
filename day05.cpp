7743aec6185e45408687c70439ac6a3c860868b1
vbenv <tjdcksl20@gmail.com>
2024-01-29 14:59:42 +0900

    v1.9 day05

--- day05.cpp
+++ day05.cpp
@@ -1,30 +1,20 @@
-//array
 #include <iostream>
 using namespace std;
 
 int main()
 {
-	//declare
-	const int CAPACITY = 10;
-	int numbers[CAPACITY];
-	int size;
-	//사용자에게 크기 입력받고 유효성 검사
-	do
+    //declare
+    int numberOfDays[13] = {0, 31, 28, 31, 30, 31, 30,
+                            31, 31, 30, 31, 30, 31}; 
+							//0을 띄워서 1~12월까지 입력받음.
+	int month;
+	
+	do // 사용자에게 달을 입력받아야 하므로 do-while
 	{
-		cout << "insert size(1~10): ";
-		cin >> size;
-	} while (size < 1 || size > CAPACITY);
-	//정수 입력 받기
-	cout << size << " 개의 숫자를 입력해주세요: ";
-	for(int i = 0; i < size ; i++)
-	{
-		cin >> numbers[i];
-	}
-	//reverse
-	cout << "입력한 숫자들을 거꾸로 출력: ";
-	for(int i = size - 1; i>=0; i--)
-	{
-		cout << numbers[i] << " ";
-	}
+	    cout << "input month(1~12): ";
+		cin >> month;
+	} while (month < 1 || month > 12); // d
+	cout << "this month " << numberOfDays[month];
+	cout << "days";
 	return 0;
 }
