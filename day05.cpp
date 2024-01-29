0d815ce077e3fdb92ce05840846e86e6e00118a3
vbenv <tjdcksl20@gmail.com>
2024-01-29 16:34:54 +0900

    v2.3 day05

--- day05.cpp
+++ day05.cpp
@@ -1,4 +1,4 @@
-// Array 값 탐색
+// Array 값 swap
 #include <iostream>
 using namespace std;
 
@@ -6,30 +6,13 @@ int main()
 {
 	//declare
 	int numArray[10] = {14, 76, 80, 33, 21, 96, 22, 88, 31, 16};
-    bool found = false;
-	int value;
-	int index;
-
-	//사용자가 입력한 값으로 할당
-	cout << "Insert number that you need: ";
-	cin >> value;
-
-
-	for(int i = 0; (i < 10)&&(!found); i++)
-	{
-		if(numArray[i] == value)
-		{
-			index = i;
-			found = true;
-		}
-	}
-	if(found)
-	{
-		cout << "The value was found at index: " << index;
-	}
-	else
-	{
-		cout << "The value was not found.";
-	}
+    
+	
+	int duck =  numArray[1];
+	numArray[1] = numArray[2];
+	numArray[2] = duck;
+	cout << numArray[1] << endl;//원래 : 76
+	cout << numArray[2] << endl;//원래 : 80
+	cout << duck;
 	return 0;
 }
