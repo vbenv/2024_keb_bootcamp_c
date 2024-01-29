c4e74edae714c9ceb4e8a0cca37f689e17d0874d
vbenv <tjdcksl20@gmail.com>
2024-01-29 16:29:42 +0900

    v2.2 day05

--- day05.cpp
+++ day05.cpp
@@ -1,20 +1,35 @@
+// Array 값 탐색
 #include <iostream>
 using namespace std;
 
 int main()
 {
 	//declare
-    int numbers[10] = {2,2,3,4,5,6,1,2,3,3}; // 배열 선언
+	int numArray[10] = {14, 76, 80, 33, 21, 96, 22, 88, 31, 16};
+    bool found = false;
+	int value;
+	int index;
 
-	double average; // 합계 변수 선언
-	int sum = 0; // sum 초기화
-	for(int i = 0; i < 10; i++) // 0<i<10 -> 10개
+	//사용자가 입력한 값으로 할당
+	cout << "Insert number that you need: ";
+	cin >> value;
+
+
+	for(int i = 0; (i < 10)&&(!found); i++)
 	{
-		sum += numbers[i];
+		if(numArray[i] == value)
+		{
+			index = i;
+			found = true;
+		}
 	}
-	average = static_cast<double>(sum) / 10; // static_cast<바꿀 자료형>(대상)
-	//int sum -> double sum으로 바꾸는 까닭은 10으로 나눴을 때
-	//좀 더 정확한 값을 출력하는 float형으로 바꾸기 위함
-	cout << average;
+	if(found)
+	{
+		cout << "The value was found at index: " << index;
+	}
+	else
+	{
+		cout << "The value was not found.";
+	}
 	return 0;
 }
