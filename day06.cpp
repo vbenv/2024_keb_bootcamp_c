7ff02c3c217f75bc3c20de17bf0d4aef86881334
vbenv <tjdcksl20@gmail.com>
2024-01-30 16:11:10 +0900

    v2.8 day06

--- day06.cpp
+++ day06.cpp
@@ -1,15 +1,54 @@
+// 힙에 동적으로 래기드 배열을
+// 할당해서 이항계수를 찾는 프로그램
+// 각 행에 할당된 메모리 위치는 행의 수보다 1만큼 큼
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main()
 {
-    int numbers[5] = {10, 2, 3, 5, 1};
-    cout << numbers<<endl; // 배열의 명만 호출하면 배열의 주소가 호출
-    cout << numbers+1<<endl;
-    
-    cout << *(numbers)<<endl;
-    cout << *(numbers+4)<<endl;
-    cout << numbers+4 << endl;
-    cout << &*(numbers+4)<<endl;
+    //선언
+    int maxPower = 10;
+    int n;
+    //입력 유효성 검사
+    do
+    {
+        cout << "(x + y)^n에서 n을 입력하세요 ";
+        cin >> n;
+    } while (n < 0 || n > maxPower);
+    //힙에 동적으로 할당.
+    int** pascal = new int*[n + 1];
+    for(int i = 0; i < n+1; i++)
+    {
+        pascal[i] = new int[i];
+    }
+    //이항계수 구하기
+    for(int i = 0; i <= n; i++)
+    {
+        for(int j = 0; j < i + 1; j++)
+        {
+            if(j == 0||i == j)
+            {
+                pascal[i][j] = 1;
+            }
+            else
+            {
+                pascal[i][j] = pascal[i - 1][j -1 ] + pascal[i -1][j];
+            }
+        }
+    }
+    //이항계수 출력
+    cout << "(x+y)^" << n << "의 이항계수: ";
+    for(int j = 0; j<= n; j++)
+    {
+        cout << setw(5) << pascal[n][j];
+    }
+    cout << endl;
+    //힙에서 제거
+    for(int i = 0; i < n+1; i++)
+    {
+        delete pascal[i];
+    }
+    delete[] pascal;
     return 0;
 }
