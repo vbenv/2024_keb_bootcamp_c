11c9719c862f9d7c1b4999f1201cca3ba9d8cfa4
vbenv <tjdcksl20@gmail.com>
2024-01-30 13:18:10 +0900

    v2.7 day06

--- day06.cpp
+++ day06.cpp
@@ -3,13 +3,13 @@ using namespace std;
 
 int main()
 {
-    int score = 92;
-    int&rscore = score;
-    cout << score <<": score "<< endl;
-    cout << rscore <<": rscore "<< endl;
+    int numbers[5] = {10, 2, 3, 5, 1};
+    cout << numbers<<endl; // 배열의 명만 호출하면 배열의 주소가 호출
+    cout << numbers+1<<endl;
     
-    rscore = 50;
-    cout << score <<": score "<< endl;
-    cout << rscore <<": rscore "<< endl;
+    cout << *(numbers)<<endl;
+    cout << *(numbers+4)<<endl;
+    cout << numbers+4 << endl;
+    cout << &*(numbers+4)<<endl;
     return 0;
 }
